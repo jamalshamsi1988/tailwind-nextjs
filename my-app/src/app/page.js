e17bd5cb08d306ae0dc254724d6571755157da6c
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2024-02-09 23:54:45 +0330

    use tailwind css

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,8 +1,13 @@
 
 export default function Home() {
   return (
-    <h1 className="text-green-700">
+  <div>
+      <h1 className="bg-green-700 text-white">
     Hello world!
   </h1>
+  <h1 className="text-red-700">
+    Hello world!
+  </h1>
+  </div>
      );
 }
